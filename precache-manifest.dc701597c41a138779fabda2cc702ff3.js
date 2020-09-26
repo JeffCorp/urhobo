e8f17289a4f1cd4044d9f41743262fd302e3a2a2
JeffCorp <patrickjuniorukutegbe@rocketmail.com>
2020-09-26 13:04:41 +0100

    Updates

---
+++ precache-manifest.dc701597c41a138779fabda2cc702ff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86fa9d70de06966ddf448a39d09373b",
+    "url": "/urhobo/index.html"
+  },
+  {
+    "revision": "a2d1ddc24d2ddaac7214",
+    "url": "/urhobo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a16c337d4ffb6af59fe1",
+    "url": "/urhobo/static/js/2.68504703.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/urhobo/static/js/2.68504703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d1ddc24d2ddaac7214",
+    "url": "/urhobo/static/js/main.095ea3ae.chunk.js"
+  },
+  {
+    "revision": "92c9c1c27fa96e2c44db",
+    "url": "/urhobo/static/js/runtime-main.5b64cd1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/urhobo/static/media/logo.5d5d9eef.svg"
+  }
+]);
